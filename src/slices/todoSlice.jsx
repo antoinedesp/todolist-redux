07205c4fdfc300ead623ad57df40aa67970a67c1
antoinedesp <114112600+antoinedesp@users.noreply.github.com>
2023-12-07 17:05:34 +0100

    Add localstorage

--- src/slices/todoSlice.jsx
+++ src/slices/todoSlice.jsx
@@ -1,9 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const saveToStorage = (toSave) => {
+  localStorage.setItem("todos", JSON.stringify(toSave));
+};
+
 export const todoSlice = createSlice({
     name: 'todo',
     initialState: {
-        value: [
+        value: JSON.parse(localStorage.getItem("todos")) ?? [
             {
                 value: 'Aller a la piscine',
                 checked: false,
@@ -24,6 +28,7 @@ export const todoSlice = createSlice({
     reducers: {
         addTodo: (state, action) => {
             state.value.push(action.payload);
+            saveToStorage(state.value);
         },
         markTodoAsDone: (state, action) => {
             let item = state.value.find((value) => {
@@ -32,6 +37,7 @@ export const todoSlice = createSlice({
             if(item) {
                 item.checked = true;
             }
+            saveToStorage(state.value);
         },
         markToDoAsUndone: (state, action) => {
             let item = state.value.find((value) => {
@@ -40,11 +46,13 @@ export const todoSlice = createSlice({
             if(item) {
                 item.checked = false;
             }
+            saveToStorage(state.value);
         },
         removeTodo: (state, action) => {
             state.value = state.value.filter((value) => {
                return !(value.value === action.payload);
             });
+            saveToStorage(state.value);
         }
     }
 });
